880d90a8d00e2091b46308189703283f0c27d2e0
Ahsan <ahsan.aasim@gmail.com>
2023-03-06 21:50:59 +0600

    code restructured

---
+++ QuranReader.py
@@ -0,0 +1,21 @@
+import xml.etree.ElementTree as et
+class QuranReader:
+    def __init__(self):
+        self.tree = et.parse('en.ahmedali.xml')
+        self.root = self.tree.getroot()
+
+    def getAyah(self, surahIndex, ayahIndex):
+        target = {
+            'surahIndex': surahIndex,
+            'surah': 'not-found',
+            'ayahIndex': ayahIndex,
+            'ayah': 'not-found',
+        }
+        for child in self.root:
+            if (int(child.attrib['index']) == int(surahIndex)):
+                target['surah'] = child.attrib['name']
+                for ayah in child:
+                    if (int(ayah.attrib['index']) == int(ayahIndex)):
+                        target['ayah'] = ayah.attrib['text']
+                        return target
+        return target
--- test.py
+++ test.py
@@ -1,23 +1,5 @@
-import xml.etree.ElementTree as et
-tree = et.parse('en.ahmedali.xml')
-root = tree.getroot()
+from QuranReader import QuranReader
 
-def getAyah(surahIndex, ayahIndex):
-    target = {
-        'surahIndex': surahIndex,
-        'surah': 'not-found',
-        'ayahIndex': ayahIndex,
-        'ayah': 'not-found',
-    }
-    for child in root:
-        if (int(child.attrib['index']) == int(surahIndex)):
-            target['surah'] = child.attrib['name']
-            for ayah in child:
-                if (int(ayah.attrib['index']) == int(ayahIndex)):
-                    target['ayah'] = ayah.attrib['text']
-                    return target
-    return target
- 
-        
-ayah = getAyah(2,66)
+quran = QuranReader()        
+ayah = quran.getAyah(2,66)
 print(ayah)
