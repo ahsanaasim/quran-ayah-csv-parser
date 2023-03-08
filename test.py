fb84115a56414cb2e44af75ab5bcb83516bba6e8
“Jonayed” <mdjonayed64173@gmail.com>
2023-03-08 14:20:41 +0600

    file creation,case handled

--- test.py
+++ test.py
@@ -1,5 +1,50 @@
+import os.path
+import re
 from QuranReader import QuranReader
+# opening a file
+# file1 = open("last read aya.txt", 'w')
+# file1.write("2,2")
+# file1.close()
+f = open("last read aya.txt", 'r')
+content = f.read()
 
-quran = QuranReader()        
-ayah = quran.getAyah(2,66)
-print(ayah)
+
+def isValidSuraAya():
+    f = open("last read aya.txt", 'r')
+    content = f.read()
+    x = re.findall("\d,\d", content)
+    if x:
+        return True
+    return False
+
+
+def converSuraAyahNumber():
+    if content:
+        l = []
+        l = content.split(',')
+        sura = int(l[0])
+        ayah = int(l[1])
+        return sura, ayah
+    return (1,1)
+    
+def getFirstAyah():
+    quran = QuranReader()
+    return quran.getAyah(1, 1)
+
+def getAyah():
+    result = converSuraAyahNumber()
+    sura = int(result[0])
+    ayah = int(result[1])
+    quran = QuranReader()
+    return quran.getAyah(sura, ayah)
+
+if os.path.exists("last read aya.txt"):
+    if isValidSuraAya():
+        ayah = getAyah()
+        print(ayah)
+    else:
+        ayah = getFirstAyah()
+        print(ayah)
+else:
+    ayah = getFirstAyah()
+    print(ayah)
